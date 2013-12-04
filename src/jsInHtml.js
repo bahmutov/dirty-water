bf47169da28d843fd1e43bdcb9c47b512ff67c71
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-12-03 19:29:55 -0500

    code between script lines

--- src/jsInHtml.js
+++ src/jsInHtml.js
@@ -1,13 +1,28 @@
 var fs = require('fs');
 var check = require('check-types');
+var S = require('string');
+var color = require('cli-color');
 
 function measureJsInHtml(options) {
   check.verify.unemptyString(options.filename, 'missing filename in '
     + JSON.stringify(options, null, 2));
 
   var src = fs.readFileSync(options.filename, 'utf-8');
-  var scriptTagRegExp = /<script[\w\W]*>[\w\W]*<\/script>/gi;
-  console.log('matches for\n' + src + '\n' + scriptTagRegExp.exec(src));
+  var n = S(src).lines().length;
+
+  var scriptTagRegExp = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  // console.log('matches for\n' + src);
+
+  var foundScript;
+  var count = 0;
+  while ((foundScript = scriptTagRegExp.exec(src)) !== null) {
+    var str = foundScript[0];
+    // console.log('found at', scriptTagRegExp.lastIndex + '\n' + str);
+    count += S(str).lines().length;
+  }
+
+  console.log(options.filename, color.green(n), 'lines',
+    count ? color.redBright(count) : color.green(count), 'js lines');
 }
 
 module.exports = measureJsInHtml;
